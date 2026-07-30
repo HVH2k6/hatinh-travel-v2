364e3a7891dd109c1b8a7610b5e03f48ae7b921c
HVH2k6 <hahung09092006@gmail.com>
2026-07-31 06:43:34 +0700

    viết lại api p3, fix vercel

--- app/[locale]/blog/page.tsx
+++ app/[locale]/blog/page.tsx
@@ -32,7 +32,7 @@ export default async function BlogListPage({ params }: { params: Promise<{ local
           </div>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {blogs.map((blog) => (
+            {blogs.map((blog: any) => (
               <BlogCard key={blog.id} item={blog} />
             ))}
           </div>
